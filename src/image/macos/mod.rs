72768645bfb18a7ad49cd66bafc39dffe221cf98
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-03-06 04:51:34 +0800

    refactor: remove extra vec
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- src/image/macos/mod.rs
+++ src/image/macos/mod.rs
@@ -5,7 +5,7 @@ pub struct Builder;
 
 impl builder::Builder for Builder {
     fn mapper() -> builder::SectionMapper {
-        vec![
+        &[
             ("__text", BaseAddresses::set_text),
             ("__eh_frame", BaseAddresses::set_eh_frame),
             ("__got", BaseAddresses::set_got),

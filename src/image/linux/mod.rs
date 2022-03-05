f7dd03909347292e0d1d9ec462e40a7132d1a0e6
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-03-06 04:48:15 +0800

    refactor: remove extra boxing
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- src/image/linux/mod.rs
+++ src/image/linux/mod.rs
@@ -6,10 +6,10 @@ pub struct Builder;
 impl builder::Builder for Builder {
     fn mapper() -> builder::SectionMapper {
         vec![
-            (".text", Box::new(BaseAddresses::set_text)),
-            (".eh_frame", Box::new(BaseAddresses::set_eh_frame)),
-            (".eh_frame_hdr", Box::new(BaseAddresses::set_eh_frame_hdr)),
-            (".got", Box::new(BaseAddresses::set_got)),
+            (".text", BaseAddresses::set_text),
+            (".eh_frame", BaseAddresses::set_eh_frame),
+            (".eh_frame_hdr", BaseAddresses::set_eh_frame_hdr),
+            (".got", BaseAddresses::set_got),
         ]
     }
 }

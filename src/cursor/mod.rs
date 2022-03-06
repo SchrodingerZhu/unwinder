3683a6a753e316a0bad23ef5d5cb96520420c38c
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-03-06 16:35:07 +0800

    Cursor refactor (#16)
    * refactor: move cursor to separate dir
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>
    
    * refactor: make state as a trait
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>
    
    * chores: format
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>
    
    * chores: prepare for eval
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- src/cursor/mod.rs
+++ src/cursor/mod.rs
@@ -1,92 +1,43 @@
+use crate::cursor::state::CursorState;
 use crate::image::ImageReader;
 use crate::{GlobalContext, SymbolInfo, UnwindError};
-use gimli::{CfaRule, Reader, Register, RegisterRule, StoreOnHeap, UnwindContext, UnwindContextStorage, UnwindSection, UnwindTableRow};
+use gimli::{
+    CfaRule, Reader, Register, RegisterRule, StoreOnHeap, UnwindContext, UnwindContextStorage,
+    UnwindSection, UnwindTableRow,
+};
 use libc::ucontext_t;
 use nix::errno::Errno;
+use std::borrow::Borrow;
 use std::mem::MaybeUninit;
 
+mod state;
+
 struct InlineStorage;
-
-#[cfg(target_arch = "x86_64")]
-mod x86_64 {
-    use gimli::{CfaRule, Register, RegisterRule, UnwindContextStorage};
-    use crate::UnwindError;
-
-    #[derive(Copy, Clone)]
-    pub struct FrameState {
-        rip: usize,
-        rsp: usize,
-    }
-
-    impl FrameState {
-        pub fn new(uctx: &libc::ucontext_t) -> Self {
-            Self {
-                rip: uctx.uc_mcontext.gregs[libc::REG_RIP as usize] as _,
-                rsp: uctx.uc_mcontext.gregs[libc::REG_RSP as usize] as _,
-            }
-        }
-
-        pub fn get_program_counter(&self) -> usize {
-            self.rip
-        }
-
-        fn get_frame_pointer(&self) -> usize {
-            self.rsp
-        }
-
-        // only use this after calling recover_frame_pointer
-        pub fn step<R, S>(&mut self, row: &gimli::UnwindTableRow<R, S>) -> Result<(), UnwindError>
-            where
-                R: gimli::Reader,
-                S: UnwindContextStorage<R>
-        {
-            match row.cfa() {
-                CfaRule::RegisterAndOffset { register, offset } => {
-                    if register.0 == 7 as u16 {
-                        self.rsp = (self.rsp as i64 + offset) as usize;
-                        match row.register(Register(16)) {
-                            RegisterRule::Undefined => {}
-                            RegisterRule::SameValue => {}
-                            RegisterRule::Offset(offset) => unsafe {
-                                self.rip = *((self.rsp as i64 + offset) as usize as *mut usize);
-                                return Ok(());
-                            }
-                            RegisterRule::ValOffset(_) => {}
-                            RegisterRule::Register(_) => {}
-                            RegisterRule::Expression(_) => {}
-                            RegisterRule::ValExpression(_) => {}
-                            RegisterRule::Architectural => {}
-                        }
-                    }
-                    Err(UnwindError::UnwindLogicalError("finished".to_string()))
-                }
-                CfaRule::Expression(_) => {
-                    todo!()
-                }
-            }
-        }
-    }
-}
-
-#[cfg(target_arch = "x86_64")]
-pub use x86_64::*;
 
 impl<R: Reader> UnwindContextStorage<R> for InlineStorage {
     type Rules = [(Register, RegisterRule<R>); 192];
     type Stack = [UnwindTableRow<R, Self>; 32];
 }
 
-struct UnwindCursor<'a, S: UnwindContextStorage<ImageReader<'a>>> {
+struct UnwindCursor<'a, Storage, State>
+where
+    Storage: UnwindContextStorage<ImageReader<'a>>,
+    State: CursorState,
+{
     global_ctx: &'a GlobalContext<'a>,
-    local_ctx: UnwindContext<ImageReader<'a>, S>,
-    state: FrameState,
+    local_ctx: UnwindContext<ImageReader<'a>, Storage>,
+    state: State,
 }
 
-trait Unwinding<'a, S: UnwindContextStorage<ImageReader<'a>>>: Sized {
-    fn local_context_mut(&mut self) -> &mut UnwindContext<ImageReader<'a>, S>;
-    fn state_mut(&mut self) -> &mut FrameState;
-    fn local_context(&self) -> &UnwindContext<ImageReader<'a>, S>;
-    fn state(&self) -> &FrameState;
+trait Unwinding<'a, Storage, State>: Sized
+where
+    Storage: UnwindContextStorage<ImageReader<'a>>,
+    State: CursorState,
+{
+    fn local_context_mut(&mut self) -> &mut UnwindContext<ImageReader<'a>, Storage>;
+    fn state_mut(&mut self) -> &mut State;
+    fn local_context(&self) -> &UnwindContext<ImageReader<'a>, Storage>;
+    fn state(&self) -> &State;
     fn global_context(&self) -> &'a GlobalContext<'a>;
 
     fn new(g_ctx: &'a GlobalContext<'a>) -> Result<Self, UnwindError> {
@@ -100,10 +51,13 @@ trait Unwinding<'a, S: UnwindContextStorage<ImageReader<'a>>>: Sized {
     fn from_ucontext(g_ctx: &'a GlobalContext<'a>, u_ctx: libc::ucontext_t) -> Self;
 
     fn get_sym_info(&self) -> SymbolInfo<'a> {
-        self.global_context().resolve_symbol(self.state().get_program_counter())
+        self.global_context()
+            .resolve_symbol(self.state().get_program_counter())
     }
 
-    fn setup_unwind_info(&mut self) -> Result<&UnwindTableRow<ImageReader<'a>, S>, UnwindError> {
+    fn setup_unwind_info(
+        &mut self,
+    ) -> Result<&UnwindTableRow<ImageReader<'a>, Storage>, UnwindError> {
         let pc = self.state().get_program_counter();
         if let Some(img) = self.global_context().find_image(pc) {
             let address = pc as u64 - img.bias as u64;
@@ -136,28 +90,33 @@ trait Unwinding<'a, S: UnwindContextStorage<ImageReader<'a>>>: Sized {
     fn next(&mut self) -> Result<(), UnwindError> {
         let mut state = *self.state();
         {
+            let context = self.global_context().borrow();
             let unwind_info = self.setup_unwind_info()?;
-            state.step(&unwind_info)?;
+            state.step(&unwind_info, context)?;
         }
         *self.state_mut() = state;
         Ok(())
     }
 }
 
-impl<'a, S: UnwindContextStorage<ImageReader<'a>>> Unwinding<'a, S> for UnwindCursor<'a, S> {
-    fn local_context_mut(&mut self) -> &mut UnwindContext<ImageReader<'a>, S> {
+impl<'a, Storage, State> Unwinding<'a, Storage, State> for UnwindCursor<'a, Storage, State>
+where
+    Storage: UnwindContextStorage<ImageReader<'a>>,
+    State: CursorState,
+{
+    fn local_context_mut(&mut self) -> &mut UnwindContext<ImageReader<'a>, Storage> {
         &mut self.local_ctx
     }
 
-    fn state_mut(&mut self) -> &mut FrameState {
+    fn state_mut(&mut self) -> &mut State {
         &mut self.state
     }
 
-    fn local_context(&self) -> &UnwindContext<ImageReader<'a>, S> {
+    fn local_context(&self) -> &UnwindContext<ImageReader<'a>, Storage> {
         &self.local_ctx
     }
 
-    fn state(&self) -> &FrameState {
+    fn state(&self) -> &State {
         &self.state
     }
 
@@ -169,24 +128,25 @@ impl<'a, S: UnwindContextStorage<ImageReader<'a>>> Unwinding<'a, S> for UnwindCu
         Self {
             global_ctx: g_ctx,
             local_ctx: Default::default(),
-            state: FrameState::new(&u_ctx),
+            state: State::new(&u_ctx),
         }
     }
 }
 
-type DynamicUnwindCursor<'a> = UnwindCursor<'a, StoreOnHeap>;
-type StaticUnwindCursor<'a> = UnwindCursor<'a, InlineStorage>;
+type DynamicUnwindCursor<'a, State> = UnwindCursor<'a, StoreOnHeap, State>;
+type StaticUnwindCursor<'a, State> = UnwindCursor<'a, InlineStorage, State>;
 
 #[cfg(test)]
 mod test {
     extern crate rustc_demangle;
+    use crate::cursor::state::FramePointerBasedState;
     use crate::cursor::Unwinding;
     use crate::{Frame, GlobalContext};
 
     #[test]
     fn it_inits_cursor() {
         let g = GlobalContext::new();
-        let mut cursor = super::DynamicUnwindCursor::new(&g).unwrap();
+        let mut cursor = super::DynamicUnwindCursor::<FramePointerBasedState>::new(&g).unwrap();
         while let Ok(_) = cursor.next() {
             let sym = cursor.get_sym_info();
             println!("AVMA: {:?}", sym.avma);
@@ -206,7 +166,7 @@ mod test {
                             Some(map.to_string())
                         }
                     })
-                    .map(|x|rustc_demangle::demangle(&x).to_string())
+                    .map(|x| rustc_demangle::demangle(&x).to_string())
                     .collect::<Vec<_>>()
             );
             println!()
---
+++ src/cursor/state/mod.rs
@@ -0,0 +1,64 @@
+use crate::{GlobalContext, UnwindError};
+use gimli::{EvaluationResult, Register, RegisterRule, UnwindContextStorage};
+use std::borrow::Borrow;
+
+#[cfg(target_arch = "x86_64")]
+mod x86_64;
+
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::*;
+
+pub trait CursorState: Sized + Copy + Clone {
+    fn new(u_ctx: &libc::ucontext_t) -> Self;
+    fn get_program_counter(&self) -> usize;
+    fn get_register(&self, reg: Register) -> Result<usize, UnwindError>;
+
+    fn get_cfa<R, S>(
+        &self,
+        row: &gimli::UnwindTableRow<R, S>,
+        g_ctx: &GlobalContext,
+    ) -> Result<usize, UnwindError>
+    where
+        R: gimli::Reader,
+        S: UnwindContextStorage<R>;
+
+    fn step<R, S>(
+        &mut self,
+        row: &gimli::UnwindTableRow<R, S>,
+        g_ctx: &GlobalContext,
+    ) -> Result<(), UnwindError>
+    where
+        R: gimli::Reader,
+        S: UnwindContextStorage<R>;
+
+    fn recover_register<R, S>(
+        &self,
+        reg: Register,
+        row: &gimli::UnwindTableRow<R, S>,
+        g_ctx: &GlobalContext,
+    ) -> Result<usize, UnwindError>
+    where
+        R: gimli::Reader,
+        S: UnwindContextStorage<R>,
+    {
+        self.get_cfa(row, g_ctx)
+            .and_then(|cfa| match row.register(reg) {
+                RegisterRule::Undefined => Err(UnwindError::UnwindEnded),
+                RegisterRule::SameValue => self.get_register(reg),
+                RegisterRule::Offset(offset) => unsafe {
+                    Ok(*((cfa as i64 + offset) as usize as *mut usize))
+                },
+                RegisterRule::ValOffset(offset) => Ok((cfa as i64 + offset) as usize),
+                RegisterRule::Register(target) => self.get_register(target),
+                RegisterRule::Expression(expr) => {
+                    todo!()
+                }
+                RegisterRule::ValExpression(_) => {
+                    todo!()
+                }
+                RegisterRule::Architectural => Err(UnwindError::NotSupported(
+                    "target register recovery is architectural",
+                )),
+            })
+    }
+}

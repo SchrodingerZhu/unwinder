803dd9fb16905a43ac8a628bbadcf7e334ed69f9
anqurvanillapy <anqurvanillapy@gmail.com>
2022-03-06 22:03:08 +0800

    chore: Notes about `getcontext` on macOS

--- src/cffi/macos.rs
+++ src/cffi/macos.rs
@@ -1,5 +1,7 @@
 use libc::{c_int, ucontext_t};
 
 extern "C" {
+    // XXX: Deprecated on macOS actually, do not expect this to run.  If it
+    // doesn't, use some easy assembly to retrieve the registers.
     pub fn getcontext(ucp: *mut ucontext_t) -> c_int;
 }
